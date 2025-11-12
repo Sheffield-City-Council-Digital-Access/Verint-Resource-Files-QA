8c4a60fbb8ddbb2e7124ee7fc77279f7f1b58282
Elliott Griffiths <99219139+Elliott-Griffiths@users.noreply.github.com>
2025-11-12 17:23:59 +0000

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2949,15 +2949,50 @@ function getAndSetReviewPageData() {
             fieldLabel = $(`#dform_widget_label_${fieldName}`).text();
             fieldValue = formatDateTime(KDF.getVal(fieldName)).uk.date;
           } else if (fieldType === "file") {
-            fieldLabel = $(`#dform_widget_label_${fieldName}`).text();
-            fieldValue = KDF.getVal(
-              fieldName.replace("file_", "txt_file_name_")
-            );
-            const filePath = KDF.getVal(
-              fieldName.replace("file_", "txt_file_path_")
-            );
-            if (KDF.kdf().access === "agent" && filePath) {
-              fieldValue = `<a href="${filePath}" target="_blank">${fieldValue}</a>`;
+            if (KDF.kdf().form.name === "blue_badge_application") {
+              fieldLabel = $(`#dform_widget_label_${fieldName}`).text();
+              let fileControlName = fieldName.replace("file_", "");
+              const fileNameField = "txt_file_name_" + fileControlName;
+              const filePathField = "txt_file_path_" + fileControlName;
+
+              const fileNamesString = KDF.getVal(fileNameField);
+              const filePathsString = KDF.getVal(filePathField);
+
+              fieldValue = fileNamesString;
+              if (KDF.kdf().access === "agent" && filePathsString) {
+
+                const fileNames = fileNamesString.split(",");
+                const filePaths = filePathsString.split(",");
+                if (
+                  fileNames.length === filePaths.length &&
+                  fileNames.length > 0
+                ) {
+                  let linkedFiles = [];
+                  for (let i = 0; i < filePaths.length; i++) {
+                    const name = fileNames[i].trim();
+                    const path = filePaths[i].trim();
+
+                    if (name && path) {
+                      linkedFiles.push(
+                        `<a href="${path}" target="_blank">${name}</a>`
+                      );
+                    }
+                  }
+
+                  fieldValue = linkedFiles.join("<br>");
+                }
+              }
+            } else {
+              fieldLabel = $(`#dform_widget_label_${fieldName}`).text();
+              fieldValue = KDF.getVal(
+                fieldName.replace("file_", "txt_file_name_")
+              );
+              const filePath = KDF.getVal(
+                fieldName.replace("file_", "txt_file_path_")
+              );
+              if (KDF.kdf().access === "agent" && filePath) {
+                fieldValue = `<a href="${filePath}" target="_blank">${fieldValue}</a>`;
+              }
             }
           } else {
             if (fieldClass.indexOf("currency") !== -1) {
